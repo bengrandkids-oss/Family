5acd06e95963acbc09a48f7e2c4a6ac4e61c4220
thelaughingbett <114688637+thelaughingbett@users.noreply.github.com>
2026-02-06 18:05:37 +0300

    Modify family_members_table structure
    Added 'family_name' column and changed 'gender' to enum type.
    i feel like the date of birth lazima iongezwe 
    plus the correction to enum ndio gender ikue only female or male

--- database/migrations/2026_02_06_122828_create_family_members_table.php
+++ database/migrations/2026_02_06_122828_create_family_members_table.php
@@ -20,8 +20,9 @@ return new class extends Migration
             
             $table->string('first_name');
             $table->string('last_name')->nullable();
-            $table->string('gender', 10)->nullable();
-            $table->date('date_of_birth')->nullable();
+            $table->string("family_name');
+            $table->enum('gender', ["male", "female"]);
+            $table->date('date_of_birth');
             $table->date('date_of_death')->nullable();
             $table->string('photo')->nullable();
             
